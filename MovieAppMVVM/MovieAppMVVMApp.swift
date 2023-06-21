7f263169f2705ac42f824bb98ebd9222935a57af
Franklin Velasquez <iefranklin@hotmail.es>
2023-06-20 22:05:40 -0600

    Initial Commit

---
+++ MovieAppMVVM/MovieAppMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppMVVMApp.swift
+//  MovieAppMVVM
+//
+//  Created by Franklin Velásquez on 20/06/23.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
